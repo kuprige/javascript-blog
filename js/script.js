5d31c2de343e53fdeeb5c68ab1ec7cbaf97906af
Monika Kupracz <kupraczmonika@gmail.com>
2023-03-15 22:04:05 +0100

    Add this to the code

--- js/script.js
+++ js/script.js
@@ -3,33 +3,32 @@
 // console.log('links:', links);
 // });
 
-const titleClickHandler = function (event) {
-  console.log("Link was clicked!");
-  console.log(event);
+const titleClickHandler = function(event){
+  const clickedElement = this;
+  console.log('Link was clicked!');
 
-  const activeLinks = document.querySelectorAll(".titles a.active");
+  /* [DONE] remove class 'active' from all article links  */
 
-  for (let activeLink of activeLinks) {
-    activeLink.classList.remove("active");
+  const activeLinks = document.querySelectorAll('.titles a.active');
+
+  for(let activeLink of activeLinks){
+    activeLink.classList.remove('active');
   }
 
-  /* add class 'active' to the clicked link */
+  clickedElement.classList.add('active');
+  console.log('clickedElement:', clickedElement);
 
-  const activeArticles = document.querySelectorAll(".post.active");
-
-  for (let activeArticle of activeArticles) {
-    activeArticle.classList.remove("active");
-  }
+  /* [DONE] remove class 'active' from all articles */
 
   /* get 'href' attribute from the clicked link */
 
   /* find the correct article using the selector (value of 'href' attribute) */
 
   /* add class 'active' to the correct article */
-};
+}
 
-const links = document.querySelectorAll(".titles a");
+const links = document.querySelectorAll('.titles a');
 
-for (let link of links) {
-  link.addEventListener("click", titleClickHandler);
+for(let link of links){
+  link.addEventListener('click', titleClickHandler);
 }
